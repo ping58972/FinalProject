5c2962a457f471078349593b740e67ef89c52d17
SKaelble <44001789+SKaelble@users.noreply.github.com>
2018-11-03 12:01:59 -0500

    Began Outline of GUI Package

---
+++ src/GUI/QuestionScene.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import edu.century.finalproject.BTNode;
+import edu.century.finalproject.ResponseLinkedQueue;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class QuestionScene extends Scene{
+
+	public QuestionScene() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	private BTNode questionTree;
+	private ResponseLinkedQueue responseQueue;
+	
+	
+	
+	public void addResponse() {
+		
+	}
+}
---
+++ src/GUI/VeteranBenefitsApplication.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VeteranBenefitsApplication extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		QuestionScene questionScene = new QuestionScene();
+		
+	}
+
+}
