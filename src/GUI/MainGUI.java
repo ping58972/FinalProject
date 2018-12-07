1b8cb2918ce28b34f33d2aa2fe6e470860ea9781
Ping58972 <ping58972@gmail.com>
2018-12-07 16:52:27 -0600

    Building Main GUI

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -1,5 +1,35 @@
 package GUI;
+/**public class MainGUI from the package GUI
+ * 
+ *  Century College, CSCI 2082 Fall 2018.
+ *  VeteranEmail.java, Programming Final Project.
+ *  
+ *  @author (Ping) Nalongsone Danddank
+ *  @version 1.0
+ *  @since 12/06/2018
+ * */
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Button;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.io.File;
 public class MainGUI {
 
 }
