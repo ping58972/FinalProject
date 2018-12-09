9efb8dedc44fe486d7488b509f6c094dea24d135
Schusswill <Will.schuss@gmail.com>
2018-12-09 11:53:18 -0600

    Another attempt at emaining

--- src/emailing/emailUtil.java
+++ src/emailing/emailUtil.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -39,27 +40,43 @@ public class EmailUtil {
 		}
 	}
 
+	public static void sendTLSEmail(String fromEmail, String password, String toEmail,
+			String subject, String bodyText) {
+		Properties props = System.getProperties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(fromEmail, password);
+					}
+		});
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(fromEmail));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+			message.setSubject(subject);
+			message.setText(bodyText);
+
+			Transport.send(message);
+
+
+		}catch(MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static void main(String[]args) {
 		final String password = "qvPK4nUmFc2W";
 		final String fromEmail = "smtpjavatest@gmial.com";
 		final String toEmail = "will.schuss@gmail.com";
 
-		System.out.println("SSLEmail Start");
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com"); //smtp
-		props.put("mail.smtp.socketFactory.port", "465"); //SSL
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+		sendTLSEmail(fromEmail, password, toEmail, "Test", "this is a Test");
 
-		Authenticator auth = new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(fromEmail,password);
-			}
-		};
 
-		Session session = Session.getInstance(props,auth);
-		System.out.println("Session created");
-		EmailUtil.sendEmail(session, toEmail,"SSLEmail testing Subject","SSLEmail Testing body");
 	}
 }
