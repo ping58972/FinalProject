afeada74928b3c12f7f3d3d0fec825b009ec21c5
Schusswill <Will.schuss@gmail.com>
2018-12-10 14:23:22 -0600

    removed un-used statement so sean can commit

--- src/GUI/ResultView.java
+++ src/GUI/ResultView.java
@@ -86,9 +86,6 @@ public class ResultView extends VBox {
 		//NEED VETERANEMAIL CLASS AND MAINGUI IMPELEMENTED - COMMENTED OUT SO CODE IS TESTABLE
 		emailBtn.setOnAction(e ->{
 			createPDF();
-			EmailUtil.sendAttachment(EmailUtil.getTLSSession(), "smtpJavaTest@gmail.com", "vet benifits Email", /*add body text object here*/"Testing", System.getProperty("user.home")+ "/Downloads/VET_Benift.pdf");
-
-
 
 		});
 
