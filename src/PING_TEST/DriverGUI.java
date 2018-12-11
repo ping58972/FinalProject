3a6945abfdff05be5b03a358b0d670f6b4c482de
Ping58972 <ping58972@gmail.com>
2018-12-10 23:17:43 -0600

    PING_TEST for DriverGUI.java file

--- src/PING_TEST/DriverGUI.java
+++ src/PING_TEST/DriverGUI.java
@@ -71,10 +71,10 @@ public class DriverGUI extends JFrame implements ActionListener {
 		btnStart.setEnabled(false);
 		setVisible(true);
 	}
-/*public static void main(String[] args) {
+public static void main(String[] args) {
 				
 	DriverGUI driver = new DriverGUI();
-	}*/
+	}
 
 @Override
 public void actionPerformed(ActionEvent e) {
